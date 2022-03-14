d86adfc5762d72111de160dabb6cbe3b67e6ca7a
Chuan Ou Yang <Chuan.Ou.Yang@ibm.com>
2022-03-14 10:25:48 +0800

    fix test

--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -11,12 +11,12 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PriorityQueueTest {
     static Stream<Arguments> streamProvider(){
         return Stream.of(
-                Arguments.of(new int[]{3, 1, 2}, new int[]{1, 2, 3, 4}),
-                Arguments.of(new int[]{-3, -1, -2, 5}, new int[]{-3, -2, -1, 5, 6}),
-                Arguments.of(new int[]{3, -2, -5, -1, 2}, new int[]{-5, -2, -1, 2, 3, 4}),
-                Arguments.of(new int[]{-3, 1, 11, 0, 9, 3}, new int[]{-3, 0, 1, 3, 9, 11, 12}),
-                Arguments.of(new int[]{3, 7, 2, -1, -2}, new int[]{-2, -1, 2, 3, 7, 8}),
-                Arguments.of(new int[]{5, 4, 2, 3}, new int[]{2, 3, 4, 5, 6})
+                Arguments.of(new int[]{3, 1, 2}, new int[]{1, 2, 3}),
+                Arguments.of(new int[]{-3, -1, -2, 5}, new int[]{-3, -2, -1, 5}),
+                Arguments.of(new int[]{3, -2, -5, -1, 2}, new int[]{-5, -2, -1, 2, 3}),
+                Arguments.of(new int[]{-3, 1, 11, 0, 9, 3}, new int[]{-3, 0, 1, 3, 9, 11}),
+                Arguments.of(new int[]{3, 7, 2, -1, -2}, new int[]{-2, -1, 2, 3, 7}),
+                Arguments.of(new int[]{5, 4, 2, 3}, new int[]{2, 3, 4, 5})
         );
     }
 
