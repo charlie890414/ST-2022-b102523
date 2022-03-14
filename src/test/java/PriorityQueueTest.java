45d6ecf76f11f519672d1a12997cbb1cad4b7895
Chuan Ou Yang <Chuan.Ou.Yang@ibm.com>
2022-03-14 10:03:44 +0800

    init

---
+++ src/test/java/PriorityQueueTest.java
@@ -0,0 +1,72 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.PriorityQueue;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PriorityQueueTest {
+    static Stream<Arguments> streamProvider(){
+        return Stream.of(
+                Arguments.of(new int[]{3, 1, 2}, new int[]{1, 2, 3}),
+                Arguments.of(new int[]{-3, -1, -2, 5}, new int[]{-3, -2, -1, 5}),
+                Arguments.of(new int[]{3, -2, -5, -1, 2}, new int[]{-5, -2, -1, 2, 3}),
+                Arguments.of(new int[]{-3, 1, 11, 0, 9 ,3}, new int[]{-3, 0, 1, 3, 9, 11}),
+                Arguments.of(new int[]{3, 7, 2, -1, -2}, new int[]{-2, -1, 2, 3 ,7}),
+                Arguments.of(new int[]{5, 4, 2, 3}, new int[]{2, 3, 4, 5})
+        );
+    }
+
+    @ParameterizedTest(name = "#{index} - Test with argument={0},{1}")
+    @MethodSource("streamProvider")
+    public void PriorityQueueTest_RunTest(int[] random_array, int[] correct_array){
+        PriorityQueue<Integer> testQueue = new PriorityQueue<Integer>();
+        int[] result = new int[random_array.length];
+
+        for(int value: random_array){
+            testQueue.add(value);
+        }
+
+        for(int i=0; testQueue.size() != 0; i++){
+            result[i] = testQueue.poll();
+        }
+
+        assertArrayEquals(correct_array, result);
+    }
+
+    @Test
+    public void whenExceptionThrown_thenOfferNull(){
+        Exception exception = assertThrows(NullPointerException.class, () ->{
+            PriorityQueue<Integer> testQueue = new PriorityQueue<Integer>();
+            testQueue.offer(null);
+        });
+
+        assertTrue(exception.getMessage() == null);
+    }
+
+    @Test
+    public void whenExceptionThrown_thenInitialCapacityNotGreaterThenOne(){
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->{
+            PriorityQueue<Integer> testQueue = new PriorityQueue<Integer>(0);
+        });
+
+        assertTrue(exception.getMessage() == null);
+    }
+
+    @Test
+    public void whenExceptionThrown_thenCannotCastDifferentObject(){
+        Exception exception = assertThrows(ClassCastException.class, () ->{
+            PriorityQueue<Object> testQueue = new PriorityQueue<Object>();
+            testQueue.add(123);
+            testQueue.add("123");
+        });
+
+        String expectedMessage = "cannot be cast";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+}
